0a48d248c1fddfc2a1ad5d5bdfc82ad8afb74db6
RovinYu <rovinyu@gmail.com>
2018-07-20 20:12:16 +0800

    Add redisdemo project

---
+++ redisdemo/src/test/java/com/rovin/redisdemo/RedisdemoApplicationTests.java
@@ -0,0 +1,34 @@
+package com.rovin.redisdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RedisdemoApplicationTests {
+
+    public static Logger logger= LoggerFactory.getLogger(RedisdemoApplicationTests.class);
+
+    @Test
+    public void contextLoads() {
+    }
+
+    @Autowired
+    RedisDao redisDao;
+
+    @Test
+    public void testRedis() {
+        redisDao.setKey("name", "David");
+        redisDao.setKey("age", "12");
+
+        logger.info(redisDao.getValue("name"));
+        logger.info(redisDao.getValue("age"));
+    }
+}
